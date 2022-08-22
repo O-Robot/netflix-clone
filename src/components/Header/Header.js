181d9ebf9043d45deae69b2a62a9175e64258a1e
O-Robot <adewaleogooluwani@gmail.com>
2022-08-22 16:01:46 +0100

    new update

---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import "./Header.css";
+
+const Header = () => {
+  return (
+    // <header className="showcase">
+    //     <div className='showcase-top'>
+    //         <img src="https://i.ibb.co/r5krrdz/logo.png" alt="logo" />
+    //         <Link to="/" className='btn btn-rounded'>
+    //                 Sign In
+    //         </Link>
+    //     </div>
+    //     <div className='showcase-content'>
+    //         <h1>Unlimited movies, TV shows and more</h1>
+    //         <p>Watch anywhere. Cancel Anytime</p>
+    //         <Link to="/shows" className='btn btn-xq'>Watch Free For 30 Days
+    //         <i className='fas fa-chevron-right btn-icon'></i>
+    //         </Link>
+    //     </div>
+    // </header>
+    <p style={{color:'red'}}>Header</p>
+  );
+}
+
+export default Header;
